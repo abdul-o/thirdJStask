243511e53fa043a1fa9ad13483b14856b2c01012
abdul-o <abdullateefdauda01@gmail.com>
2020-04-19 22:57:02 +0100

    so tough

---
+++ index.js
@@ -0,0 +1,31 @@
+   
+   function replace ( wholeNumber){
+     for ( let i= 1; i <= wholeNumber; i++){
+       
+       if( i % 2 === 0 && i % 3 === 0){
+          console.log("yu-gi");
+        }
+        else if ( i % 2 === 0 && i % 5 === 0){
+           console.log("yu-oh");
+        }
+        else if( i % 3 === 0 && i % 5 === 0){
+           console.log("gi-oh");
+        }
+       else if (i % 2 === 0 ){
+          console.log("yu");
+     }
+       else if( i % 3 === 0){
+          console.log("gi");
+       }
+       else if ( i % 5 === 0){
+         console.log("oh");
+       }
+      
+       else {
+           console.log(i)
+       }
+      }
+     };
+
+    replace(20);
+    replace(10);
